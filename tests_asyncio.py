5e84cce6f8f306d14a4e35eea7094b65f53134ce
Rodrigo Tobar <rtobar@icrar.org>
2020-02-24 15:59:59 +0800

    Using single type of reader in async tests
    The code now automatically adjusts the type of reader depending on the
    type of data used by each backend, so there's no further need to do it
    manually in the tests.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- tests_asyncio.py
+++ tests_asyncio.py
@@ -7,11 +7,8 @@ import io
 
 
 class AsyncReader(object):
-    def __init__(self, data, use_str):
-        if use_str:
-            self.data = io.StringIO(data.decode('utf8'))
-        else:
-            self.data = io.BytesIO(data)
+    def __init__(self, data):
+        self.data = io.BytesIO(data)
 
     async def read(self, n=-1):
         return self.data.read(n)
@@ -28,36 +25,32 @@ class Base(object):
 
     def test_async_basic_parse(self):
         async def run():
-            reader = AsyncReader(JSON, self.backend.__name__.endswith('.python'))
             events = []
-            async for event in self.backend.basic_parse_async(reader):
+            async for event in self.backend.basic_parse_async(AsyncReader(JSON)):
                 events.append(event)
             self.assertEqual(JSON_EVENTS, events)
         self.loop.run_until_complete(run())
 
     def test_async_parse(self):
         async def run():
-            reader = AsyncReader(JSON, self.backend.__name__.endswith('.python'))
             events = []
-            async for event in self.backend.parse_async(reader):
+            async for event in self.backend.parse_async(AsyncReader(JSON)):
                 events.append(event)
             self.assertEqual(JSON_PARSE_EVENTS, events)
         self.loop.run_until_complete(run())
 
     def test_async_items(self):
         async def run():
-            reader = AsyncReader(JSON, self.backend.__name__.endswith('.python'))
             objects = []
-            async for obj in self.backend.items_async(reader, ''):
+            async for obj in self.backend.items_async(AsyncReader(JSON), ''):
                 objects.append(obj)
             self.assertEqual([JSON_OBJECT], objects)
         self.loop.run_until_complete(run())
 
     def test_async_kvitems(self):
         async def run():
-            reader = AsyncReader(JSON, self.backend.__name__.endswith('.python'))
             objects = []
-            async for obj in self.backend.kvitems_async(reader, 'docs.item'):
+            async for obj in self.backend.kvitems_async(AsyncReader(JSON), 'docs.item'):
                 objects.append(obj)
             self.assertEqual(JSON_KVITEMS, objects)
         self.loop.run_until_complete(run())
