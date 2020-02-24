855b8b2af0e7882f2c7a4b70d0d17ac2b8a2038b
Rodrigo Tobar <rtobar@icrar.org>
2020-02-24 15:45:05 +0800

    Preparing for file opening mode checking
    We do a check on how file-like objects were opened in the case of the
    original synchronous to automatically correct the type of data they read
    (ijson expects bytes mostly, but some users still give string readers,
    which is mostly important in python 3). These new functions will allow
    us to do the same in our *_async functions.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- ijson/utils35.py
+++ ijson/utils35.py
@@ -3,8 +3,36 @@ Python3.5+ specific utilities
 '''
 import collections
 
-from ijson import utils, common
+from ijson import utils, common, compat
 
+
+class utf8reader_async(compat.utf8reader):
+    """
+    Takes a utf8-encoded string asynchronous reader and asynchronously reads
+    bytes out of it
+    """
+    async def read(self, n):
+        data = await self.str_reader.read(n)
+        return data.encode('utf-8')
+
+class string_reader_async(compat.utf8reader):
+    """
+    Takes a utf8-encoded string asynchronous reader and asynchronously reads
+    bytes out of it
+    """
+    async def read(self, n):
+        data = await self.str_reader.read(n)
+        return data.decode('utf-8')
+
+async def _check_file_reader(f, use_string_reader):
+    """Returns an asynchronous file-like object that reads bytes"""
+    if use_string_reader:
+        if type(await f.read(0)) == compat.bytetype:
+            f = string_reader_async(f)
+        return f
+    if type(await f.read(0)) == compat.bytetype:
+        return f
+    return compat._warn_and_return(utf8reader_async(f))
 
 class sendable_deque(collections.deque):
     '''Like utils.sendable_list, but for deque objects'''
