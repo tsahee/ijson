b29313bf1875704d78a21724a393785248ad64a7
Rodrigo Tobar <rtobar@icrar.org>
2020-08-03 16:15:53 +0800

    Add missing long_description_content_type
    This was flagged as a small note/warning by twine.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setupArgs = dict(
     license = 'BSD',
     description = 'Iterative JSON parser with a standard Python iterator interface',
     long_description = open('README.rst').read(),
+    long_description_content_type = 'text/x-rst',
 
     classifiers = [
         'Development Status :: 5 - Production/Stable',
