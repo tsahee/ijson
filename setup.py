e5f90687b65020715d0a04a7afba853489233310
Rodrigo Tobar <rtobar@icrar.org>
2020-08-03 16:16:32 +0800

    Move yajl patching process to setup.py
    When building ijson in yajl embedded mode (IJSON_EMBED_YAJL=1) we
    previously had to manually get a copy of yajl, and move files around so
    yajl and ijson could be compiled together into a single shared library.
    This made it less clear that these two processes had to be performed,
    making the setup process a bit more difficult to maintain.
    
    This commit brings the patching process of yajl into setup.py. Instead
    of getting a copy of yajl via git clone we use the new git submodule as
    the yajl sources, which we first copy into a writable location, and then
    modify them as needed. This means that users who want to build an ijson
    package in yajl embedded mode only need to ensure that the submodule is
    present, and that the environment variable is set.
    
    This commit also updates the AppVeyor pipeline to use this new
    procedure.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import distutils.sysconfig
 import glob
 import os
 import platform
+import shutil
 import tempfile
 
 from setuptools import setup, find_packages, Extension
@@ -82,6 +83,20 @@ def yajl_present():
         if os.path.exists(fname):
             os.remove(fname)
 
+
+def patch_yajl_sources():
+    """Make yajl sources ready for direct compilation against them"""
+    # cp cextern/yajl -R $yajl_sources_copy
+    # mkdir $yajl_sources_copy/yajl
+    # cp $yajl_sources_copy/src/api/*.h $yajl_sources_copy/yajl
+    patched_sources = os.path.join(tempfile.mkdtemp(), 'yajl')
+    shutil.copytree(os.path.join('cextern', 'yajl'), patched_sources)
+    headers_original = os.path.join(patched_sources, 'src', 'api')
+    headers_copy = os.path.join(patched_sources, 'yajl')
+    shutil.copytree(headers_original, headers_copy)
+    return patched_sources
+
+
 # Conditional compilation of the yajl_c backend
 if platform.python_implementation() == 'CPython':
     extra_sources = []
@@ -91,9 +106,10 @@ if platform.python_implementation() == 'CPython':
     if not embed_yajl:
         have_yajl = yajl_present()
     else:
-        extra_sources = sorted(glob.glob(os.path.join('yajl', 'src', '*.c')))
-        extra_sources.remove(os.path.join('yajl', 'src', 'yajl_version.c'))
-        extra_include_dirs = ['yajl', os.path.join('yajl', 'src')]
+        yajl_sources = patch_yajl_sources()
+        extra_sources = sorted(glob.glob(os.path.join(yajl_sources, 'src', '*.c')))
+        extra_sources.remove(os.path.join(yajl_sources, 'src', 'yajl_version.c'))
+        extra_include_dirs = [yajl_sources, os.path.join(yajl_sources, 'src')]
         libs = []
     if embed_yajl or have_yajl:
         yajl_ext = Extension('ijson.backends._yajl2',
